b7f5f83754cf7e993c8a6b75fa7dd7dbbd6d0604
SerafimovichEugene <eugene.serafimovich@gmail.com>
2017-04-22 17:21:33 +0300

    fix: gulpfile, bootstrap-custom

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,28 @@ var plumber = require('gulp-plumber');
 var watch = require('gulp-watch');
 
 gulp.task('sass', function () {
-    gulp.src('scss/main.scss')
+    gulp.src('./scss/main.scss')
         .pipe(plumber())
         .pipe(sass({
             errLogToConsole: true
         }))
-        .pipe(gulp.dest('/css'));
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('bootstrap-sass', function () {
+    gulp.src('scss/bootstrap-custom.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('default', function () {
     gulp.run('sass');
+    gulp.run('bootstrap-sass');
     gulp.watch('scss/**', function (event) {
         gulp.run('sass');
+        gulp.run('bootstrap-sass'); 
     })
 });
